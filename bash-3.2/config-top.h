c2d622f4cb8969594c0bdc14a8a9f80246ae3c2c
nubecoder <nubecoder@gmail.com>
2011-11-21 04:01:50 -0500

    Some path changes to bash-3.2.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- bash-3.2/config-top.h
+++ bash-3.2/config-top.h
@@ -52,14 +52,14 @@
 /* The default value of the PATH variable. */
 #ifndef DEFAULT_PATH_VALUE
 #define DEFAULT_PATH_VALUE \
-  "/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:."
+  "/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:."
 #endif
 
 /* The value for PATH when invoking `command -p'.  This is only used when
    the Posix.2 confstr () function, or CS_PATH define are not present. */
 #ifndef STANDARD_UTILS_PATH
 #define STANDARD_UTILS_PATH \
-  "/bin:/usr/bin:/sbin:/usr/sbin:/etc:/usr/etc"
+  "/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:/system/etc"
 #endif
 
 /* Default primary and secondary prompt strings. */
@@ -73,10 +73,10 @@
 #define KSH_COMPATIBLE_SELECT
 
 /* System-wide .bashrc file for interactive shells. */
-#define SYS_BASHRC "/etc/bash.bashrc"
+#define SYS_BASHRC "/system/etc/bash.bashrc"
 
 /* System-wide .bash_logout for login shells. */
-/* #define SYS_BASH_LOGOUT "/etc/bash.bash_logout" */
+#define SYS_BASH_LOGOUT "/system/etc/bash.bash_logout"
 
 /* Define this to make non-interactive shells begun with argv[0][0] == '-'
    run the startup files when not in posix mode. */
